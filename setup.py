2e6d716686b7c5139d41e637d9bd1fe78cff1f84
Mohamed Ashkan <65010721+Mohammedashkan@users.noreply.github.com>
2023-12-26 13:47:52 +0530

    Completed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Trust-Bank-Customer-Support-ChatBot',
+    version= '0.0.0',
+    author= 'Mohamed Ashkan',
+    author_email= 'ashkan.mohammed98@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
